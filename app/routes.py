0b4107b5c26e9e4cbe912e333041b40bc99728b0
SpencerHancock <spencerjhancock@gmail.com>
2018-08-22 15:16:14 -0700

    had to fix a bunch of bugs and make everything loof prettier

--- app/routes.py
+++ app/routes.py
@@ -1,29 +1,99 @@
-from app import app
-from flask import render_template, redirect, flash
-from app.forms import LoginForm
+from app import app, db
+from flask import render_template, redirect, flash, url_for, request
+from app.forms import RegistrationForm,  LoginForm, EditProfileForm
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import User, Post
+from werkzeug.urls import url_parse
+from datetime import datetime
 
+
+@app.before_request
+def before_request():
+	if current_user.is_authenticated:
+		current_user.last_seen = datetime.utcnow()
+		db.session.commit()
 
 @app.route("/")
 @app.route("/index")
 def index():
-	user = {"username":"spencer"}
 	return render_template("index.html")
 
 @app.route("/home")
+@login_required
 def home():
-	user = {"username":"spencer"}
-	users = [
-		{"username":"bill","description" : "I am a strong, burly gay man"},
-		{"username":"carol","description" : "I am a forty year old woman looking for my first man"}
+	posts = Post.query.all()
+	return render_template("homepage.html", posts=posts, curent_user=current_user)
 
-		 	]
-	return render_template("homepage.html", user=user, users=users)
-
-@app.route("/login", methods=["POST", "GET"])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-	form = LoginForm()
-	if form.validate_on_submit():
-		flash("{} tried to login with {} password".format(form.username.data, form.password.data))
-		return redirect("/home")
-	return render_template("login.html", form=form)
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = 'home'
+        return redirect(url_for(next_page))
+    return render_template('login.html', title='Sign In', form=form)
 
+@app.route("/logout")
+@login_required
+def logout():
+	logout_user()
+	return redirect(url_for("index"))
+
+@app.route("/profile/<username>")
+@login_required
+def profile(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template("profilepage.html", current_user = current_user, user=user)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+	if current_user.is_authenticated:
+		return redirect(url_for("home"))
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		user = User(username=form.username.data, email =form.email.data)
+		user.set_password(form.password.data)
+		db.session.add(user)
+		db.session.commit()
+		flash("congrats, you've made an account!")
+		return redirect(url_for("login"))
+
+	return render_template("register.html", form=form )
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+	user = User.query.filter_by(username=username).first_or_404()
+	posts = [
+	{'author': user, 'body': 'Test post #1'},
+    {'author': user, 'body': 'Test post #2'}
+	]
+
+	return render_template('user.html', user=user, posts=posts)
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+
+
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('profile', username=current_user.username))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
